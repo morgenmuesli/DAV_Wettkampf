538d391d5c8df956ef0e9b5680bd5c542d4c1e2f
Christoph Meyer <c.meyer@emplify-software.de>
2023-07-30 13:16:02 +0200

    Initial commit

---
+++ frontend/src/application/app2.js
@@ -0,0 +1,7 @@
+// import scss entry file here for app2.js
+
+
+
+window.document.addEventListener("DOMContentLoaded", function () {
+  window.console.log("dom ready 2");
+});
---
+++ training_logger/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TrainingLoggerConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'training_logger'
